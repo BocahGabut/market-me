100bc68564ea8e3b249b876940fd5d1d69f32285
unknown <randkill.leader@gmail.com>
2020-10-13 06:30:20 +0700

    Updates

---
+++ precache-manifest.631b60ced1818ef3fdafcf315bcc8844.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36277dcdbc6e0ca1e151070ed12581e5",
+    "url": "/market-me/index.html"
+  },
+  {
+    "revision": "3fb95aa33adde2abd717",
+    "url": "/market-me/static/css/2.d2f0eb39.chunk.css"
+  },
+  {
+    "revision": "ca48147960c21b2e2131",
+    "url": "/market-me/static/css/main.af17c3b8.chunk.css"
+  },
+  {
+    "revision": "3fb95aa33adde2abd717",
+    "url": "/market-me/static/js/2.de89d3c3.chunk.js"
+  },
+  {
+    "revision": "d9b15855a077efbcb0021d6fd80c20f2",
+    "url": "/market-me/static/js/2.de89d3c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca48147960c21b2e2131",
+    "url": "/market-me/static/js/main.d148eb42.chunk.js"
+  },
+  {
+    "revision": "0f186dbe661d648ab4ae",
+    "url": "/market-me/static/js/runtime-main.27731e78.js"
+  },
+  {
+    "revision": "9069a55f567d7e3c3e61510f8d58cee6",
+    "url": "/market-me/static/media/logo.9069a55f.png"
+  }
+]);
